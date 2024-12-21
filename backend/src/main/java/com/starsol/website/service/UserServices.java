3f6007beed7b83fbc738f19f81212f908e8b870e
Daniel Leon <daniel.leon@starsolutionspty.com>
2024-12-21 10:34:00 -0500

    add Redis integration with token management and update .gitignore

--- backend/src/main/java/com/starsol/website/service/UserServices.java
+++ backend/src/main/java/com/starsol/website/service/UserServices.java
@@ -1,10 +1,29 @@
 package com.starsol.website.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServices {
-    //aqui hacer la interaccion con la base de datos
+    //interaccion con redisConfig
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
+
+    public void saveToken(String key, String token)
+    {
+        redisTemplate.opsForValue().set(key, token);
+    }
+
+    public String getToken(String key)
+    {
+        return (String) redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteToken(String key)
+    {
+        redisTemplate.delete(key);
+    }
 
     // por ahora voy a quemar el user y password y token
     public boolean validateUser(String username, String password, String token) {
